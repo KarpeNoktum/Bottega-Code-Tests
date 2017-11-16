d9015668340b574ba2b0979c6cba4b54aeca3642
Josh Beere <officialkarpenoktum@gmail.com>
2017-11-16 00:01:32 -0500

    Added string parser file

---
+++ string_parser.rb
@@ -0,0 +1,24 @@
+string_array = [
+  "Web IconHTML & CSS100%",
+  "Command LineLearn the Command Line100%",
+  "Ruby IconRuby50%",
+  "Rails IconLearn Ruby on Rails100%",
+  "Git IconLearn Git100%",
+  "SassLearn Sass20%",
+  "JQuery IconjQuery1%",
+  "Angular JSLearn AngularJS 1.X100%",
+  "Javascript IconLearn JavaScript55%"
+]
+
+
+
+def string_parser string_array
+  final_array = []
+  string_array.each do |str|
+    final_array << str.scan(/\d+/).last.to_i
+  end
+  final_array
+
+end
+
+string_parser string_array
